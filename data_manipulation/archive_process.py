655765af9d3a258daa2df5deec1e2ec9c8e3be92
Bogdan Sitaru <bogdan.sitaru@outlook.com>
2023-02-09 17:17:50 +0000

    archive split ofi files

--- data_manipulation/archive_process.py
+++ data_manipulation/archive_process.py
@@ -4,6 +4,7 @@ import py7zr
 
 from datetime import date
 from functools import partial
+from joblib import Parallel, delayed
 
 from data_manipulation.bucket_ofi import compute_bucket_ofi_from_files, get_new_bucket_ofi_file_name, \
     get_bucket_size_from_file_name, get_bucket_ofi_df, compute_bucket_ofi_df_from_bucket_ofi, BucketOFIProps
@@ -81,17 +82,32 @@ def is_valid_split_ofi_file_name(file_name: str, flt: FileFilter):
     return True
 
 
+def is_valid_split_ofi_archive(file_name: str, flt: FileFilter) -> bool:
+    if not file_name.endswith('.7z'):
+        return False
+    s = file_name[:-3].split('_')
+    if len(s) != 5:
+        return False
+    [ticker, start_date, end_date, file_type, levels] = s
+    levels = int(levels)
+    start_date = date.fromisoformat(start_date)
+    end_date = date.fromisoformat(end_date)
+
+    if not flt.filter(start_date=start_date, end_date=end_date, levels=levels, ticker=ticker):
+        return False
+    return True
+
+
 def filter_file_list(file_list: list[str], filter_fn):
     return list(filter(filter_fn, file_list))
 
 
 def process_extracted_archive(folder_path: str, out_path: str, bucket_ofi_props: BucketOFIProps,
-                              remove_after_process: bool = False) -> ():
+                              remove_after_process: bool, parallel_jobs: int) -> ():
     file_list = os.listdir(path=folder_path)
 
-    for file_name in file_list:
-        s = file_name.split('.')[0]
-        [ticker, d, _, _, file_type, lvl] = s.split('_')
+    def process_file(file_name: str):
+        [ticker, d, _, _, file_type, lvl] = file_name[:-4].split('_')
         if file_type == 'message':
             print(f"Processing {file_name}...")
             message_file_name = file_name
@@ -107,9 +123,38 @@ def process_extracted_archive(folder_path: str, out_path: str, bucket_ofi_props:
                 os.remove(message_file_path)
                 os.remove(orderbook_file_path)
 
+    Parallel(n_jobs=parallel_jobs)(delayed(process_file)(f) for f in file_list)
+
+
+def get_new_archive_name(archive_name: str, flt: FileFilter, bucket_ofi_props: BucketOFIProps):
+    [ticker, start_date, end_date, levels] = archive_name[:-3].split('_')[6: 10]
+    start_date = date.fromisoformat(start_date)
+    end_date = date.fromisoformat(end_date)
+
+    def intersect_dates(l1: date, r1: date, l2: date, r2: date):
+        if l2 is not None and l1 < l2:
+            l1 = l2
+        if r2 is not None and r2 < r1:
+            r1 = r2
+        return l1, r1
+
+    start_date, end_date = intersect_dates(start_date, end_date, flt.start_date, flt.end_date)
+    levels = int(levels)
+
+    name = f"{ticker}_{start_date}_{end_date}_SplitOFIBucket{bucket_ofi_props.bucket_size}_{levels}.7z"
+    return name
+
+
+def extract_files_from_archive(archive_path: str, out_path: str, filter_fn) -> ():
+    archive = py7zr.SevenZipFile(archive_path)
+    file_list = archive.getnames()
+    file_list = filter_file_list(file_list, filter_fn=filter_fn)
+    archive.extract(path=out_path, targets=file_list)
+
 
 def process_archive_folder(folder_path: str, temp_path: str, out_path: str, flt: FileFilter,
-                           bucket_ofi_props: BucketOFIProps, remove_after_process: bool = False) -> ():
+                           bucket_ofi_props: BucketOFIProps, remove_after_process: bool, create_archive: bool,
+                           parallel_jobs: int) -> ():
     folder_path = os.path.abspath(folder_path)
     temp_path = os.path.abspath(temp_path)
 
@@ -127,28 +172,40 @@ def process_archive_folder(folder_path: str, temp_path: str, out_path: str, flt:
             else:
                 os.mkdir(temp_folder)
                 archive_path = os.path.join(folder_path, archive_name)
-                archive = py7zr.SevenZipFile(archive_path)
-                file_list = archive.getnames()
-                file_list = filter_file_list(file_list,
-                                             filter_fn=partial(is_valid_data_file_name, flt=flt))
-                archive.extract(path=temp_folder, targets=file_list)
+                extract_files_from_archive(archive_path=archive_path, out_path=temp_folder,
+                                           filter_fn=partial(is_valid_data_file_name, flt=flt))
 
-            process_extracted_archive(folder_path=temp_folder, out_path=out_path, bucket_ofi_props=bucket_ofi_props,
-                                      remove_after_process=remove_after_process)
+            if create_archive:
+                new_archive_name = get_new_archive_name(archive_name=archive_name, flt=flt,
+                                                        bucket_ofi_props=bucket_ofi_props)
+                new_out_path = os.path.join(out_path, new_archive_name[:-3])  # Without .7z extension
+                if not os.path.exists(new_out_path):
+                    os.mkdir(new_out_path)
+
+            out_folder = new_out_path if create_archive else out_path
+
+            process_extracted_archive(folder_path=temp_folder, out_path=out_folder, bucket_ofi_props=bucket_ofi_props,
+                                      remove_after_process=remove_after_process, parallel_jobs=parallel_jobs)
             if remove_after_process:
                 shutil.rmtree(temp_folder)
+
+            if create_archive:
+                print(f"Creating archive {new_archive_name}...")
+                new_archive_path = os.path.join(out_path, new_archive_name)
+                archive = py7zr.SevenZipFile(new_archive_path, 'w')
+                file_list = os.listdir(new_out_path)
+                for file_name in file_list:
+                    archive.write(os.path.join(new_out_path, file_name), arcname=file_name)
+                archive.close()
+                if remove_after_process:
+                    shutil.rmtree(new_out_path)
         except FileExistsError:
             print(f"Archive is already processing? {archive_name}")
 
 
-def process_split_ofi_folder(folder_path: str, out_file: str, flt: FileFilter, bucket_ofi_props: BucketOFIProps):
+def process_split_ofi_folder(folder_path: str, out_file: str, bucket_ofi_props: BucketOFIProps):
     file_list = os.listdir(path=folder_path)
-    files = filter_file_list(file_list, filter_fn=partial(is_valid_split_ofi_file_name, flt=flt))
-
-    if os.path.exists(out_file):
-        os.remove(out_file)
-
-    for file_name in files:
+    for file_name in file_list:
         print(f"Processing {file_name}...")
         file_path = os.path.join(folder_path, file_name)
         bucket_ofi_props.prev_bucket_size = get_bucket_size_from_file_name(file_name)
@@ -159,3 +216,24 @@ def process_split_ofi_folder(folder_path: str, out_file: str, flt: FileFilter, b
             df.to_csv(out_file, mode='a', index=False, header=False)
         else:
             df.to_csv(out_file, index=False)
+
+
+def process_split_ofi_archive_folder(folder_path: str, out_file: str, temp_path: str, flt: FileFilter,
+                                     bucket_ofi_props: BucketOFIProps, remove_after_process: bool):
+    file_list = os.listdir(path=folder_path)
+    archive_list = filter_file_list(file_list, filter_fn=partial(is_valid_split_ofi_archive, flt=flt))
+
+    if os.path.exists(out_file):
+        os.remove(out_file)
+
+    for archive_name in archive_list:
+        print(f"Processing Archive {archive_name}...")
+        temp_folder = os.path.join(temp_path, archive_name)
+        if not os.path.exists(temp_folder):
+            os.mkdir(temp_folder)
+        archive_path = os.path.join(folder_path, archive_name)
+        extract_files_from_archive(archive_path=archive_path, out_path=temp_folder,
+                                   filter_fn=partial(is_valid_split_ofi_file_name, flt=flt))
+        process_split_ofi_folder(folder_path=temp_folder, out_file=out_file, bucket_ofi_props=bucket_ofi_props)
+        if remove_after_process:
+            shutil.rmtree(temp_folder)
--- main.py
+++ main.py
@@ -1,37 +1,42 @@
 from datetime import date
-from data_manipulation.archive_process import process_archive_folder, process_split_ofi_folder, FileFilter, BucketOFIProps
+from data_manipulation.archive_process import process_archive_folder, process_split_ofi_archive_folder, FileFilter, \
+    BucketOFIProps
 
 from models.split_ofi import run_model
 
 
 def process_archive():
     flt = FileFilter(start_date=date.fromisoformat('2016-03-01'),
-                     end_date=date.fromisoformat('2016-03-05'),
-                     tickers=['TFX'],
+                     end_date=date.fromisoformat('2016-04-01'),
+                     tickers=['TFX', 'PSA'],
                      levels=10)
     bucket_ofi_props = BucketOFIProps(levels=10,
                                       bucket_size=1,
                                       rounding=True)
     process_archive_folder(folder_path='./lobster_sample/test_data',
                            temp_path='./lobster_sample/temp_data',
-                           out_path='./lobster_sample/data',
+                           out_path='./lobster_sample/data2',
                            flt=flt,
                            bucket_ofi_props=bucket_ofi_props,
-                           remove_after_process=True)
+                           remove_after_process=True,
+                           create_archive=True,
+                           parallel_jobs=4)
 
 
 def process_multiday():
     flt = FileFilter(start_date=date.fromisoformat('2016-03-01'),
-                     end_date=date.fromisoformat('2016-03-05'),
-                     tickers=['TFX'],
+                     end_date=date.fromisoformat('2016-03-10'),
+                     tickers=['TFX', 'PSA'],
                      levels=10)
     bucket_ofi_props = BucketOFIProps(levels=10,
                                       bucket_size=30 * 60,
                                       rounding=True)
-    process_split_ofi_folder(folder_path='./lobster_sample/data',
-                             out_file='./lobster_sample/combined2.csv',
-                             flt=flt,
-                             bucket_ofi_props=bucket_ofi_props)
+    process_split_ofi_archive_folder(folder_path='./lobster_sample/data2',
+                                     out_file='./lobster_sample/combined2.csv',
+                                     temp_path='./lobster_sample/temp_data',
+                                     flt=flt,
+                                     bucket_ofi_props=bucket_ofi_props,
+                                     remove_after_process=True)
 
 
 def run_lr():
@@ -41,4 +46,4 @@ def run_lr():
 
 
 if __name__ == '__main__':
-    run_lr()
+    process_multiday()

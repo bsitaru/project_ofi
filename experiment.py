1fedb32ba5524ece73d68235c42c877309c16abb
Bogdan Sitaru <bogdan.sitaru@outlook.com>
2023-03-09 23:35:18 +0000

    .

--- experiment.py
+++ experiment.py
@@ -7,7 +7,8 @@ main = typer.Typer()
 
 
 @main.command()
-def price_impact_intraday(folder_path: str, temp_path: str, bucket_size: int, in_sample_size: int, os_size: int = None,
+def price_impact_intraday(folder_path: str, temp_path: str, results_path: str, bucket_size: int, in_sample_size: int,
+                          os_size: int = None,
                           rolling: int = None, tickers: str = None, start_date: str = None, end_date: str = None,
                           parallel_jobs: int = 1):
     os_size = in_sample_size if os_size is None else os_size
@@ -17,7 +18,8 @@ def price_impact_intraday(folder_path: str, temp_path: str, bucket_size: int, in
     start_date = process_date(start_date)
     end_date = process_date(end_date)
 
-    pi_intraday.run_experiment(folder_path=folder_path, temp_path=temp_path, bucket_size=bucket_size,
+    pi_intraday.run_experiment(folder_path=folder_path, temp_path=temp_path, results_path=results_path,
+                               bucket_size=bucket_size,
                                in_sample_size=in_sample_size, os_size=os_size, rolling=rolling, start_date=start_date,
                                end_date=end_date, tickers=tickers, parallel_jobs=parallel_jobs)
 

9859702d1e7b51b22ee7d4effa3f180a1379ee33
Bogdan Sitaru <bogdan.sitaru@outlook.com>
2023-05-04 22:34:03 +0100

    strategy

--- strategy/portfolio.py
+++ strategy/portfolio.py
@@ -3,45 +3,52 @@ from datetime import date
 import pandas as pd
 import numpy as np
 
+import constants
 import data_manipulation.prices as prices
 from logging_utils import log
+from data_loader.day_prices import DayPrices
+from strategy.strategy_results import StrategyResults
 
 HORIZONT = 60
-DAYS_PER_YEAR = 250
 
 
-def make_strategy_portfolio(df: pd.DataFrame, logger=None, start_date=None, end_date=None):
-    dates = np.unique(df['date'].values)
+def make_strategy_portfolio(df: pd.DataFrame, logger=None, start_date=None, end_date=None, tickers=None):
+    date_dfs = df.groupby('date')
+    dates = np.array(list(date_dfs.groups.keys()))
     if start_date is not None:
         dates = dates[start_date <= dates]
     if end_date is not None:
         dates = dates[dates <= end_date]
 
-    def solve_date(d: date):
-        date_df = df[df['date'] == d]
+    def solve_date(d: date, date_df):
+        # date_df = df[df['date'] == d]
         times = np.unique(date_df['time_now'])
-        tickers = np.unique(date_df['ticker'])
+        day_tickers = np.unique(date_df['ticker'])
 
         prices_dfs = {}
-        for t in tickers:
-            pr_df = prices.get_prices_df_for_ticker_date(ticker=t, d=d)
-            pr_df = prices.compute_additional(pr_df)
-            prices_dfs[t] = pr_df.to_numpy()
+        for t in day_tickers:
+            if tickers is not None and t not in tickers:
+                continue
+            prices_dfs[t] = DayPrices(t, d)
 
-        money = 1.0
+        all_rets = []
         for now in times:
+            if len(all_rets) == 151:
+                money = 1.1
             df_now = date_df[date_df['time_now'] == now].values.tolist()
             weights = {}
             tot_weights = 0
             for row in df_now:
                 # row = df_now.loc[df_now['ticker'] == t]
-                [_, _, t, _, y_pred, y_train_std] = row
+                [_, _, t, y_true, y_pred, y_train_std] = row
+                if tickers is not None and t not in tickers:
+                    continue
                 # y_pred = row['y_pred'].iloc[0]
                 # y_train_std = row['y_train_std'].iloc[0]
                 ret = np.exp(y_pred) - 1.0
 
                 pr_df = prices_dfs[t]
-                price_row = pr_df[pr_df[:, 0] <= now][-1]
+                price_row = pr_df.get_price_row(now)
                 # print(f"time {now}, date {d}, ticker {t} --- {price_row}")
                 rel_spread = price_row[4]
                 if np.abs(ret) <= rel_spread or y_train_std == 0:
@@ -51,6 +58,9 @@ def make_strategy_portfolio(df: pd.DataFrame, logger=None, start_date=None, end_
                 tot_weights += np.abs(w)
                 weights[t] = w
 
+                # if np.sign(y_pred) != np.sign(y_true):
+                #     print(f"negative --- ticker {t} --- weight {w} --- y_true {y_true} --- {y_pred}")
+
             if len(weights) == 0:
                 continue
 
@@ -59,35 +69,41 @@ def make_strategy_portfolio(df: pd.DataFrame, logger=None, start_date=None, end_
                 w /= tot_weights
 
                 pr_df = prices_dfs[t]
-                price_now = pr_df[pr_df[:, 0] <= now][-1]
-                price_next = pr_df[pr_df[:, 0] <= (now + HORIZONT)][-1]
+                price_now = pr_df.get_price_row(now)
+                price_next = pr_df.get_price_row(now + HORIZONT)
 
                 ret = 0.0
-                if w < 0:
-                    # ret = price_next['ask_price'] / price_now['bid_price']
-                    ret = price_next[1] / price_now[2]
-                else:
-                    # ret = price_next['bid_price'] / price_now['ask_price']
-                    ret = price_next[2] / price_now[1]
+                if w < 0:  # sell now, buy next
+                    ret = price_now[3] / price_next[3]
+                    # ret = price_now[2] / price_next[1]
+                else:  # buy now, sell next
+                    ret = price_next[3] / price_now[3]
+                    # ret = price_next[2] / price_now[1]
+
+                # if np.sign(ret - 1.0) != np.sign(ret2 - 1.0):
+                #     print(f"ret {ret} --- ret2 {ret2} --- w {w} --- price_now {price_now[1]} {price_now[2]} {price_now[3]} --- price_next {price_next[1]} {price_next[2]} {price_next[3]} --- y {y_true} {y_pred} --- spread {np.exp(y_pred) - 1.0} {price_now[4]} ")
+
+                # if ret < 1.0:
+                #     print(f"negative profit --- ticker {t} --- weight {w} --- ret {ret}")
 
                 rets.append(np.abs(w) * ret)
             tot_ret = sum(rets)
-            money *= tot_ret
+            all_rets.append(tot_ret)
 
-        money -= 1.0
-        return money
+        all_rets = np.array(all_rets)
+        return np.mean(all_rets - 1.0), np.product(all_rets) - 1.0
 
-    all_profits = []
+    all_profits_mean, all_profits_prod = [], []
     for d in dates:
-        money = solve_date(d)
-        log(f'date {d} pnl --- {money}', logger=logger)
-        all_profits.append(money)
+        if d in constants.EARLY_CLOSING_DAYS:
+            continue
+        date_df = date_dfs.get_group(d)
+        ret_mean, ret_prod = solve_date(d, date_df)
+        log(f'date {d} pnl --- {ret_mean} --- {ret_prod}')
+        all_profits_mean.append(ret_mean)
+        all_profits_prod.append(ret_prod)
 
-    all_profits = np.array(all_profits)
-    avg_profit = np.mean(all_profits)
-    sum_profit = np.sum(all_profits)
-    std = np.std(all_profits)
-    annual = avg_profit * DAYS_PER_YEAR
-    log(f"average profit --- {avg_profit} --- std: {std}", logger=logger)
-    log(f"sum profit --- {sum_profit}", logger=logger)
-    log(f"annualized profit --- {annual}", logger=logger)
+    stats_mean = StrategyResults(all_profits_mean)
+    stats_prod = StrategyResults(all_profits_prod)
+    return stats_mean, stats_prod
+

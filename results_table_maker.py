157737378c8e30d30913318d70b60b98aa66c4cb
Bogdan Sitaru <bogdan.sitaru@outlook.com>
2023-05-13 12:05:45 +0100

    config

--- results_table_maker.py
+++ results_table_maker.py
@@ -5,6 +5,7 @@ from ml_collections import ConfigDict
 
 main = typer.Typer()
 
+
 def get_is_os_from_log(log_path: str):
     r_is, r_os = None, None
     with open(log_path, 'r') as f:
@@ -15,6 +16,7 @@ def get_is_os_from_log(log_path: str):
             if l[3] == 'os_r2:':
                 r_os = round(float(l[4]), 3)
     return r_is, r_os
+
 
 def get_log_results(log_path: str):
     dict = {}
@@ -31,8 +33,9 @@ def get_log_results(log_path: str):
                 val = float(l[4])
                 std = float(l[7])
                 dict[key] = val
-                dict_std[key] = val
+                dict_std[key] = std
     return dict, dict_std
+
 
 def get_results_from_folder(path: str):
     folders = os.listdir(path)
@@ -80,23 +83,24 @@ def get_results_from_folder(path: str):
         lns.append(table_line)
     return lns
 
+
 @main.command()
 def table_maker(path: str):
     header = \
-"""\\begin{table}[]
-    \\centering
-    \\begin{tabular}{lcccccc|ll}
-    \\toprule
-        Exp & Model & Levels & PCA & Regression & Cluster Size & Data & In Sample & Out of Sample \\\\
-    \\midrule"""
+        """\\begin{table}[]
+            \\centering
+            \\begin{tabular}{lcccccc|ll}
+            \\toprule
+                Exp & Model & Levels & PCA & Regression & Cluster Size & Data & In Sample & Out of Sample \\\\
+            \\midrule"""
 
     ending = \
-"""\\bottomrule
-    \\end{tabular}
-    \\caption{Results}
-    \\label{tab.results}
-\\end{table}
-"""
+        """\\bottomrule
+            \\end{tabular}
+            \\caption{Results}
+            \\label{tab.results}
+        \\end{table}
+        """
 
     l = get_results_from_folder(path)
     answer = '\n'.join([header] + l + [ending])
@@ -119,6 +123,7 @@ def get_portfolio_results_from_log(log_path: str):
                 val = round(val, ndigits=4)
                 return val
     return None
+
 
 def get_future_results_from_folder(path: str):
     folders = os.listdir(path)
@@ -146,6 +151,8 @@ def get_future_results_from_folder(path: str):
         table_line = f"{name} & {model} & {levels} & {pca} & {regression} & {horizonts} & {r_is} & {r_os} & {pnl} \\\\"
         lns.append(table_line)
     return lns
+
+
 @main.command()
 def future_table_maker(path: str):
     header = \
@@ -167,6 +174,60 @@ def future_table_maker(path: str):
     l = get_future_results_from_folder(path)
     answer = '\n'.join([header] + l + [ending])
     print(answer)
+
+
+def transpose(l):
+    return list(map(list, zip(*l)))
+
+
+@main.command()
+def custom():
+    def get_name(l):
+        return f"individual_price_impact_issize_1800_ossize_1800_rolling_1800_seltype_OFI_lvl_{l}_volnrm_regtype_linear"
+
+    experiments = [get_name(l) for l in range(1, 11)]
+    centering = f"r|{'c' * 10}"
+
+    bk = '\\'
+    header = \
+        f"""{bk}begin{{table}}[]
+            {bk}centering
+            {bk}begin{{tabular}}{{{centering}}}
+            {bk}toprule
+                 & {' & '.join([f"${bk}modpi{{{l}}}$" for l in range(1, 11)])} {bk}{bk}
+            {bk}midrule"""
+
+    def get_rows(name):
+        path = f'./results_server/{name}'
+        dct, dct_std = get_log_results(os.path.join(path, 'logs.log'))
+
+        is_r2 = str(round(dct['in_r2'] * 100, 2))
+        std_is_r2 = "(" + str(round(dct_std['in_r2'] * 100, 2)) + ")"
+        os_r2 = str(round(dct['os_r2'] * 100, 2))
+        std_os_r2 = "(" + str(round(dct_std['os_r2'] * 100, 2)) + ")"
+
+        return [is_r2, std_is_r2, os_r2, std_os_r2]
+
+    rows = [get_rows(n) for n in experiments]
+    rows = transpose(rows)
+    rows[0] = ['IS $R^2$'] + rows[0]
+    rows[1] = [' '] + rows[1]
+    rows[2] = ['OS $R^2$'] + rows[2]
+    rows[3] = [' '] + rows[3]
+
+    ending = \
+        """\\bottomrule
+            \\end{tabular}
+            \\caption{Results}
+            \\label{tab.results}
+        \\end{table}
+        """
+
+    rows = list(map(lambda x: ' & '.join(x), rows))
+    rows = ' \\\\ \n'.join(rows)
+    answer = '\n'.join([header, rows, ending])
+    print(answer)
+
 
 if __name__ == '__main__':
     main()
